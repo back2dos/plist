ed9f7620a72d288305fb1c18cae73a38cf772281
back2dos <back2dos@gmail.com>
2014-03-31 14:23:36 +0200

    Basic functionality.

---
+++ src/plist/Parser.hx
@@ -0,0 +1,32 @@
+package plist;
+
+using tink.CoreApi;
+
+class Parser {
+	static public function parse(s:String):Dynamic 
+		return parseNode(Xml.parse(s).firstElement());
+	
+	static function val(x:Xml)
+		return x.firstChild().nodeValue;
+		
+	static function parseNode(x:Xml):Dynamic {
+		return
+			switch x.nodeName {
+				case 'plist': parseNode(x.firstElement());
+				case 'dict': 
+					var elts = x.elements(),
+						ret:Dynamic = {};
+						
+					while (elts.hasNext()) 
+						Reflect.setField(ret, val(elts.next()), parseNode(elts.next()));
+					ret;
+				case 'true': true;
+				case 'false': false;
+				case 'array': [for (c in x.elements()) parseNode(c)];
+				case 'string': val(x);
+				case 'integer': Std.parseInt(val(x));
+				case 'real': Std.parseFloat(val(x));
+				case v: null;
+			}
+	}
+}
---
+++ tests/TestParser.hx
@@ -0,0 +1,60 @@
+package ;
+
+class TestParser extends Base {
+	function testRead() {
+
+		assertStructEq({
+			Author : 'William Shakespeare',
+			Lines : ['It is a tale told by an idiot,', 'Full of sound and fury, signifying nothing.'],
+			Birthdate : 1564,
+		}, plist.Parser.parse(SHAKESPEARE));
+		
+		assertStructEq({
+			author: 'foobar',
+			displayName: 'Manuscript',
+			enabled: true,
+			identifier: 'manuscript',
+			styleSheet: 'blabla',
+			type: 'richText',
+		}, plist.Parser.parse(OTHER));
+	}
+	
+	static var SHAKESPEARE = 
+		'<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist SYSTEM "file://localhost/System/Library/DTDs/PropertyList.dtd">
+		<plist version="1.0">
+		<dict>
+		    <key>Author</key>
+		    <string>William Shakespeare</string>
+		    <key>Lines</key>
+		    <array>
+		        <string>It is a tale told by an idiot,</string>
+		        <string>Full of sound and fury, signifying nothing.</string>
+		    </array>
+		    <key>Birthdate</key>
+		    <integer>1564</integer>
+		</dict>
+		</plist>';
+
+	static var OTHER = 
+		'<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+		<plist version="1.0">
+		<dict>
+			<key>author</key>
+			<string>foobar</string>
+			<key>displayName</key>
+			<string>Manuscript</string>
+			<key>enabled</key>
+			<true/>
+			<key>identifier</key>
+			<string>manuscript</string>
+			<key>styleSheet</key>
+			<string>blabla</string>
+			<key>type</key>
+			<string>richText</string>
+		</dict>
+		</plist>';
+
+
+}
